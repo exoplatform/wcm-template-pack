2437be99f7ffceddfd6f9a756b7a2d1cebeb96cd
toannh <toannh@exoplatform.com>
2015-09-14 17:00:44 +0700

    ECMS-7199 [Copporate website 1][UI] Text on Why Us section is truncated

--- agital-template/src/main/webapp/javascript/eXo/wcm/WCMTemplatePack.js
+++ agital-template/src/main/webapp/javascript/eXo/wcm/WCMTemplatePack.js
@@ -6,9 +6,15 @@
   var WCMTemplatePack = function () {
   }
 
+  WCMTemplatePack.prototype.aInit = function(){
+    console.log("Agital init......");
+  }
 
+  gj(document).ready(function() {
+    gj(".uiContentBox").dotdotdot();
+  });
 
-
+  eXo.ecm.WCMTemplatePack = new WCMTemplatePack();
   return {
     WCMTemplatePack : eXo.ecm.WCMTemplatePack
   };
